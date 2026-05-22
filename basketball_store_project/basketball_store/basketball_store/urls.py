0e533e039b1f256a0acb0760a65e45daf6703772
Arimark <erm4kowe@yandex.ru>
2026-05-22 13:44:22 +0300

    v8

--- basketball_store_project/basketball_store/basketball_store/urls.py
+++ basketball_store_project/basketball_store/basketball_store/urls.py
@@ -50,5 +50,5 @@ urlpatterns = [
     path('sneaker/<int:pk>/delete/', views.SneakerDeleteView.as_view(), name='sneaker_delete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
